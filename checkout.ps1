3d7fe1c7c6903de3afa25e8cf6bd23674e61edde
Sergey Seroukhov <seroukhov@gmail.com>
2025-03-26 10:32:36 -0700

    Update checkout.ps1

--- checkout.ps1
+++ checkout.ps1
@@ -1,43 +1,43 @@
 #!/usr/bin/env pwsh
 
-git clone git@github.com:pip-services-samples/service-quickstart-node.git
-git clone git@github.com:pip-services-samples/service-beacons-node.git
-git clone git@github.com:pip-services-samples/client-beacons-node.git
-git clone git@github.com:pip-services-samples/sample-beacons-node.git
-git clone git@github.com:pip-services-samples/facade-sample-node.git
+git clone git@github.com:pip-services3-samples/service-quickstart-node.git
+git clone git@github.com:pip-services3-samples/service-beacons-node.git
+git clone git@github.com:pip-services3-samples/client-beacons-node.git
+git clone git@github.com:pip-services3-samples/sample-beacons-node.git
+git clone git@github.com:pip-services3-samples/facade-sample-node.git
 
-git clone git@github.com:pip-services-samples/service-quickstart-nodex.git
-git clone git@github.com:pip-services-samples/service-beacons-nodex.git
-git clone git@github.com:pip-services-samples/client-beacons-nodex.git
-git clone git@github.com:pip-services-samples/sample-beacons-nodex.git
-git clone git@github.com:pip-services-samples/facade-sample-nodex.git
+git clone git@github.com:pip-services3-samples/service-quickstart-nodex.git
+git clone git@github.com:pip-services3-samples/service-beacons-nodex.git
+git clone git@github.com:pip-services3-samples/client-beacons-nodex.git
+git clone git@github.com:pip-services3-samples/sample-beacons-nodex.git
+git clone git@github.com:pip-services3-samples/facade-sample-nodex.git
 
-git clone git@github.com:pip-services-samples/service-quickstart-python.git
-git clone git@github.com:pip-services-samples/service-beacons-python.git
-git clone git@github.com:pip-services-samples/client-beacons-python.git
-git clone git@github.com:pip-services-samples/sample-beacons-python.git
-git clone git@github.com:pip-services-samples/facade-sample-python.git
+git clone git@github.com:pip-services3-samples/service-quickstart-python.git
+git clone git@github.com:pip-services3-samples/service-beacons-python.git
+git clone git@github.com:pip-services3-samples/client-beacons-python.git
+git clone git@github.com:pip-services3-samples/sample-beacons-python.git
+git clone git@github.com:pip-services3-samples/facade-sample-python.git
 
-git clone git@github.com:pip-services-samples/service-quickstart-go.git
-git clone git@github.com:pip-services-samples/service-beacons-go.git
-git clone git@github.com:pip-services-samples/client-beacons-go.git
-git clone git@github.com:pip-services-samples/sample-beacons-go.git
-git clone git@github.com:pip-services-samples/facade-sample-go.git
+git clone git@github.com:pip-services3-samples/service-quickstart-go.git
+git clone git@github.com:pip-services3-samples/service-beacons-go.git
+git clone git@github.com:pip-services3-samples/client-beacons-go.git
+git clone git@github.com:pip-services3-samples/sample-beacons-go.git
+git clone git@github.com:pip-services3-samples/facade-sample-go.git
 
-git clone git@github.com:pip-services-samples/service-quickstart-dart.git
-git clone git@github.com:pip-services-samples/service-beacons-dart.git
-git clone git@github.com:pip-services-samples/client-beacons-dart.git
-git clone git@github.com:pip-services-samples/sample-beacons-dart.git
-git clone git@github.com:pip-services-samples/facade-sample-dart.git
+git clone git@github.com:pip-services3-samples/service-quickstart-dart.git
+git clone git@github.com:pip-services3-samples/service-beacons-dart.git
+git clone git@github.com:pip-services3-samples/client-beacons-dart.git
+git clone git@github.com:pip-services3-samples/sample-beacons-dart.git
+git clone git@github.com:pip-services3-samples/facade-sample-dart.git
 
-git clone git@github.com:pip-services-samples/service-quickstart-java.git
-git clone git@github.com:pip-services-samples/service-beacons-java.git
-git clone git@github.com:pip-services-samples/client-beacons-java.git
-git clone git@github.com:pip-services-samples/sample-beacons-java.git
-git clone git@github.com:pip-services-samples/facade-sample-java.git
+git clone git@github.com:pip-services3-samples/service-quickstart-java.git
+git clone git@github.com:pip-services3-samples/service-beacons-java.git
+git clone git@github.com:pip-services3-samples/client-beacons-java.git
+git clone git@github.com:pip-services3-samples/sample-beacons-java.git
+git clone git@github.com:pip-services3-samples/facade-sample-java.git
 
-git clone git@github.com:pip-services-samples/service-quickstart-dotnet.git
-git clone git@github.com:pip-services-samples/service-beacons-dotnet.git
-git clone git@github.com:pip-services-samples/client-beacons-dotnet.git
-git clone git@github.com:pip-services-samples/sample-beacons-dotnet.git
-git clone git@github.com:pip-services-samples/facade-sample-dotnet.git
+git clone git@github.com:pip-services3-samples/service-quickstart-dotnet.git
+git clone git@github.com:pip-services3-samples/service-beacons-dotnet.git
+git clone git@github.com:pip-services3-samples/client-beacons-dotnet.git
+git clone git@github.com:pip-services3-samples/sample-beacons-dotnet.git
+git clone git@github.com:pip-services3-samples/facade-sample-dotnet.git
